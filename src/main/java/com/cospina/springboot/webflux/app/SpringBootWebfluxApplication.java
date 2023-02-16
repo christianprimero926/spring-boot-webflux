0f521c74c7381c9d6aff1fdf52e75bc598f579f6
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-16 16:55:36 -0500

    Seccion3
    32. Eliminando colección productos (drop collection)

--- src/main/java/com/cospina/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/cospina/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.cospina.springboot.webflux.app.models.dao.ProductDao;
 import com.cospina.springboot.webflux.app.models.documents.Product;
@@ -18,6 +19,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 	@Autowired
 	private ProductDao dao;
 	
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+	
 	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 	
 	public static void main(String[] args) {
@@ -26,6 +30,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		mongoTemplate.dropCollection("productos").subscribe();
+		mongoTemplate.dropCollection("products").subscribe();
 		
 		Flux.just(
 				new Product("TV Panasonic Pantalla LCD", 456.89),
