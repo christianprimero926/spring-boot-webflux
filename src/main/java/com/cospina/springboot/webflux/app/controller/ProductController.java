23776bb58e486d17056ce19de5490bb85327298c
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-02-16 18:07:51 -0500

    Update ProductController.java
    35. Suscribiendo otro Observador y modificando el Stream reactivo

--- src/main/java/com/cospina/springboot/webflux/app/controller/ProductController.java
+++ src/main/java/com/cospina/springboot/webflux/app/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.cospina.springboot.webflux.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +14,24 @@ import reactor.core.publisher.Flux;
 
 @Controller
 public class ProductController {
-	
+
 	@Autowired
 	private ProductDao dao;
-	
-	@GetMapping({"/show_all", "/"})
-	public String showAll(Model model) {		
-		Flux<Product> products = dao.findAll();
-		
+
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+
+	@GetMapping({ "/show_all", "/" })
+	public String showAll(Model model) {
+		Flux<Product> products = dao.findAll().map(product -> {
+			product.setName(product.getName().toUpperCase());
+			return product;
+		});
+
+		products.subscribe(prod -> log.info(prod.getName()));
+
 		model.addAttribute("products", products);
 		model.addAttribute("tittle", "Listado de productos");
-		
+
 		return "show_all";
 	}
 
