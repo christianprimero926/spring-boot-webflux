20fe866f64d4cc32c66c5513f1704f6a317de2ff
Christian David Ospina Primero <christianprimero26@hotmail.com>
2023-05-12 19:20:10 -0500

    Seccion4
    51. Añadiendo categorías de ejemplo y el operador thenMany

--- src/main/java/com/cospina/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/cospina/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,7 +1,8 @@
 package com.cospina.springboot.webflux.app;
 
-import java.util.Date;
-
+import com.cospina.springboot.webflux.app.models.documents.Category;
+import com.cospina.springboot.webflux.app.models.documents.Product;
+import com.cospina.springboot.webflux.app.models.services.ProductServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,48 +10,54 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-
-import com.cospina.springboot.webflux.app.models.dao.ProductDao;
-import com.cospina.springboot.webflux.app.models.documents.Product;
-
 import reactor.core.publisher.Flux;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class SpringBootWebfluxApplication implements CommandLineRunner{
+public class SpringBootWebfluxApplication implements CommandLineRunner {
 
-	@Autowired
-	private ProductDao dao;
-	
-	@Autowired
-	private ReactiveMongoTemplate mongoTemplate;
-	
-	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebfluxApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+    @Autowired
+    private ProductServiceImpl service;
+    @Autowired
+    private ReactiveMongoTemplate mongoTemplate;
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxApplication.class, args);
+    }
 
-		mongoTemplate.dropCollection("products").subscribe();
-		
-		Flux.just(
-				new Product("TV Panasonic Pantalla LCD", 456.89),
-				new Product("Sony Camara HD Digital", 177.89),
-				new Product("Apple iPod", 46.89),
-				new Product("Sony Notebook", 846.89),
-				new Product("Hewlett Packard Multifuncional", 200.89),
-				new Product("Bianchi Bicicleta", 70.89),
-				new Product("HP Notebook Omen 17", 2500.89),
-				new Product("Mica Cómoda 5 Cajones", 150.89),
-				new Product("TV Sony Bravia OLED 4K Ultra HD", 2255.89)				
-				)
-		.flatMap(product -> {
-			product.setCreateAt(new Date());
-			return dao.save(product);	
-		})
-		.subscribe(product -> log.info("Insert: " + product.getId() + " " + product.getName()));
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+        mongoTemplate.dropCollection("products").subscribe();
+        mongoTemplate.dropCollection("categories").subscribe();
+
+        Category electronic = new Category("Electronico");
+        Category sport = new Category("Deporte");
+        Category computation = new Category("Computacion");
+        Category furniture = new Category("Muebles");
+
+        Flux.just(electronic, sport, computation, furniture)
+                .flatMap(service::saveCategory)
+                .doOnNext(c -> {
+                    log.info("Categoria creada: " + c.getName() + ", Id: " + c.getId());
+                }).thenMany(Flux.just(
+                                new Product("TV Panasonic Pantalla LCD", 456.89, electronic),
+                                new Product("Sony Camara HD Digital", 177.89, electronic),
+                                new Product("Apple iPod", 46.89, electronic),
+                                new Product("Sony Notebook", 846.89, computation),
+                                new Product("Hewlett Packard Multifuncional", 200.89, computation),
+                                new Product("Bianchi Bicicleta", 70.89, sport),
+                                new Product("HP Notebook Omen 17", 2500.89, computation),
+                                new Product("Mica Cómoda 5 Cajones", 150.89, furniture),
+                                new Product("TV Sony Bravia OLED 4K Ultra HD", 2255.89, electronic)
+                        )
+                        .flatMap(product -> {
+                            product.setCreateAt(new Date());
+                            return service.save(product);
+                        }))
+                .subscribe(product -> log.info("Insert: " + product.getId() + " " + product.getName()));
+    }
 
 }
